2dd69671df0cb3e94d35f377459cbbb3a39962e5
Hwang Inseong <88603039+inseong0910@users.noreply.github.com>
2024-03-25 22:34:14 +0900

    Create codekata_python_11.py
    파이썬 코드카타 11번 문제

---
+++ codekata_python_11.py
@@ -0,0 +1,12 @@
+# 짝수와 홀수
+# https://school.programmers.co.kr/learn/courses/30/lessons/12937?language=python3 
+
+def solution(num):
+    if num % 2 == 0:
+        return 'Even'
+    else:
+        return 'Odd'
+    return answer
+
+# 개인 문제풀이 과정
+# https://kitsch2023.tistory.com/154 
