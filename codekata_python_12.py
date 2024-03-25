747e58eb14e827a688f472bbce9249be98c782b5
Hwang Inseong <88603039+inseong0910@users.noreply.github.com>
2024-03-25 22:44:42 +0900

    Create codekata_python_12.py
    파이썬 코드카타 12번 문제

---
+++ codekata_python_12.py
@@ -0,0 +1,9 @@
+# 평균 구하기
+# https://school.programmers.co.kr/learn/courses/30/lessons/12944 
+# 주요 개념 : 평균값은 전체합계 / 길이
+
+def solution(arr):
+    return sum(arr) / len(arr) 
+
+# 개인 문제풀이 과정
+# https://kitsch2023.tistory.com/155 
