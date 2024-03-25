911d07a281191d2c18f20c2d622a1d67cf210e6d
Hwang Inseong <88603039+inseong0910@users.noreply.github.com>
2024-03-25 23:06:10 +0900

    Create codekata_python_13.py
    파이썬 코드카타 13번 문제

---
+++ codekata_python_13.py
@@ -0,0 +1,10 @@
+# 자릿수 더하기
+# https://school.programmers.co.kr/learn/courses/30/lessons/12931 
+# 주요 내용 : map() 
+
+def solution(n):
+    answer = list(map(int,str(n)))
+    return sum(answer)
+
+# 개인 문제풀이 과정
+# https://kitsch2023.tistory.com/156 
