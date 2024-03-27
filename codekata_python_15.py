e54e7e4ff1c8af5f2a144701303d02cceb366558
Hwang Inseong <88603039+inseong0910@users.noreply.github.com>
2024-03-27 20:47:33 +0900

    Create codekata_python_15.py
    파이썬 코드카타 15번 문제

---
+++ codekata_python_15.py
@@ -0,0 +1,13 @@
+# 나머지가 1이 되는 수 찾기
+# https://school.programmers.co.kr/learn/courses/30/lessons/87389 
+# 주요 개념 : for ~ break 
+
+def solution(n):
+    for i in range(1, n):
+        if n%i == 1:
+            answer = i
+            break
+    return answer 
+
+# 개인 풀이과정 url 
+# https://kitsch2023.tistory.com/159
