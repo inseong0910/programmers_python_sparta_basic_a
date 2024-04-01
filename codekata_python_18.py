0a26a8a8a42822fbe0f2ae0952ad7e0efd97a7fe
Hwang Inseong <88603039+inseong0910@users.noreply.github.com>
2024-04-01 11:48:39 +0900

    Create codekata_python_18.py
    파이썬 코드카타 18번 문제

---
+++ codekata_python_18.py
@@ -0,0 +1,10 @@
+# 문자열을 정수로 바꾸기 
+# https://school.programmers.co.kr/learn/courses/30/lessons/12925 
+# 주요 개념 : 자료형 변환
+
+def solution(s):
+    answer = int(s)
+    return answer 
+
+# 개인 문제풀이 과정
+# https://kitsch2023.tistory.com/170 
