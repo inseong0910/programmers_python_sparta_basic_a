296a71c1a4be34b6e9c17f6580f6f13712ded6c3
Hwang Inseong <88603039+inseong0910@users.noreply.github.com>
2024-04-16 09:24:01 +0900

    Create codekata_python_24.py
    파이썬 코드카타 문제 24

---
+++ codekata_python_24.py
@@ -0,0 +1,11 @@
+# 서울에서 김서방 찾기 
+# https://school.programmers.co.kr/learn/courses/30/lessons/12919 
+# 주요 개념 : a.index() 
+
+def solution(seoul):
+    answer = "김서방은 " + str(seoul.index("Kim")) + "에 있다"
+    return answer
+
+
+# 개인 문제 풀이 과정 
+# https://kitsch2023.tistory.com/193 
